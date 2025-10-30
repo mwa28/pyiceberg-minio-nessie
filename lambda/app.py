e0833115087efa58dbcc4c0e903037b2cb47d835
Mohamad Alhalabi <mh19.my@gmail.com>
2025-10-30 21:44:40 +0100

    switch to download file

--- lambda/app.py
+++ lambda/app.py
@@ -1,29 +1,31 @@
 import os
+
 import boto3
-import duckdb
+import pandas as pd
+import pyarrow as pa
 from pyiceberg.catalog import load_catalog
+
 
 def lambda_handler(event, context):
     # Extract filename from S3 event
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    key = event['Records'][0]['s3']['object']['key']
+    bucket = event["Records"][0]["s3"]["bucket"]["name"]
+    key = event["Records"][0]["s3"]["object"]["key"]
     print(f"Processing file: {key}")
 
     # Read file from MinIO (simulated S3)
     s3 = boto3.client(
-        's3',
-        endpoint_url=os.getenv('MINIO_ENDPOINT'),
-        aws_access_key_id=os.getenv('MINIO_ACCESS_KEY'),
-        aws_secret_access_key=os.getenv('MINIO_SECRET_KEY'),
+        "s3",
+        endpoint_url=os.getenv("MINIO_ENDPOINT"),
+        aws_access_key_id=os.getenv("MINIO_ACCESS_KEY"),
+        aws_secret_access_key=os.getenv("MINIO_SECRET_KEY"),
     )
-    file_obj = s3.get_object(Bucket=bucket, Key=key)
-    data = file_obj['Body'].read().decode('utf-8')
+    local_path = key.split("/")[-1]
+    s3.download_file(bucket, key, local_path)
 
-    # Transform with DuckDB
-    conn = duckdb.connect()
-    conn.execute("CREATE OR REPLACE TABLE temp_data AS SELECT * FROM read_json(?)", [data])
-    transformed = conn.execute("SELECT *, current_timestamp() as ingest_time FROM temp_data").fetchdf()
+    df = pd.read_json(local_path, lines=True)
 
+    # Calculate the difference from previous timestamp
+    df["diff_ts"] = df.groupby(["controller_id", "parameter"])["timestamp"].diff()
 
     catalog = load_catalog(
         "rest",
@@ -34,15 +36,15 @@ def lambda_handler(event, context):
             "s3.access-key-id": "minioadmin",
             "s3.secret-access-key": "minioadmin",
             "s3.path-style-access": "true",
-        }
+        },
     )
 
     catalog.create_namespace_if_not_exists("iceberg_db")
 
-   table = catalog.load_table("my_namespace.sensor_data")
-    with table.update_schema() as update:
-        update.add_column("ingest_time", "timestamp")
-    with table.update_mode("append") as writer:
-        writer.write_df(transformed)
+    table = catalog.load_table("iceberg_db.sensor_historical_data")
+    to_append = pa.Table.from_pandas(df, schema=table.schema().as_arrow())
 
-    return {"status": "success", "file": key}
+    with table.transaction() as trx:
+        trx.append(to_append)
+
+    return {"statusCode": 200, "file": key}
