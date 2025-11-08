296410979081aa3cc05a3b866b0f4c128bd61f9f
Mohamad Alhalabi <mohamad.alhalabi.ext@veolia.com>
2025-11-08 19:41:09 +0100

    add fugue

--- spark/batch_ingest.py
+++ spark/batch_ingest.py
@@ -1,5 +1,4 @@
 from fugue import transform
-from pyspark import SparkConf
 from pyspark.sql import SparkSession
 
 from common.transform import calculate_delta
@@ -7,5 +6,23 @@ from common.transform import calculate_delta
 spark = SparkSession.builder.getOrCreate()
 sc = spark.sparkContext
 
-# Query the data
-spark.sql("CREATE DATABASE icebergy")
+# Read the data
+df = spark.read.option("multiline", "true").json("s3://iot-bucket/sensor_data.jsonl")
+
+df = transform(df, calculate_delta, schema="*,diff_ts:long")
+
+df.createOrReplaceTempView("sensor_data_with_delta")
+
+spark.sql(
+    """
+  MERGE INTO iceberg_db.sensor_historical_data AS target  
+    USING sensor_data_with_delta AS source
+    ON target.controller_id = source.controller_id 
+       AND target.timestamp = source.timestamp
+       AND target.parameter = source.parameter
+    WHEN MATCHED THEN
+      UPDATE SET *
+    WHEN NOT MATCHED THEN
+      INSERT *
+"""
+)
